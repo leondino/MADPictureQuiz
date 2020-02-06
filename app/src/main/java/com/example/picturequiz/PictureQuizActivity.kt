c0f4e062a2aeaf9e33b8a54d28ebfdc3878c5f15
Léon Imanuel <leondino0309@gmail.com>
2020-02-06 14:32:10 +0100

    Refactored layout name

--- app/src/main/java/com/example/picturequiz/PictureQuizActivity.kt
+++ app/src/main/java/com/example/picturequiz/PictureQuizActivity.kt
@@ -3,13 +3,13 @@ package com.example.picturequiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_picture_quiz.*
 
 class PictureQuizActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_picture_quiz)
         btnConfirm.setOnClickListener {
             checkAnswer()
         }
